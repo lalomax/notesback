24fe2d5a3b1cb01b2aa27f604425ceb31c226e64
Lalomax <lalomax365@gmail.com>
2024-07-09 11:36:34 -0400

    initial

---
+++ index.js
@@ -0,0 +1,66 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const Note = require('./models/note')
+
+app.use(express.json());
+
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+});
+
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id).then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  }) 
+  .catch(error => next(error))
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+console.log(notes)
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body
+  console.log(body.content)
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`server runninG on port ${PORT}`);
+});
---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url =
+  //   `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://lalomax365:${password}@cluster0.arn5vqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+// const note = new Note(
+
+//   // { content: "CSS is Hard", important: true },
+//   { content: "Mongoose make things easy", important: true }
+// );
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
