96fa001bd3f0939413ddcbec33d0237143a3f30f
Lalomax <lalomax365@gmail.com>
2024-08-02 13:20:10 -0400

    feat: 4.1 - 4.7 separate in modules and unit tests

--- models/note.js
+++ models/note.js
@@ -1,24 +1,11 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-
-const url = process.env.MONGODB_URI
-
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
-  content: { type:String, minlength:5, required: true },
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
   important: Boolean,
 })
 
@@ -29,6 +16,5 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-
 
 module.exports = mongoose.model('Note', noteSchema)
